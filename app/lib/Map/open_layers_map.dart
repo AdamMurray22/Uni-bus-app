f85d4cf78f056ce9a232f59147599d9503ebb314
Adam-Murray <116890354+AdamMurrayPortsmouth@users.noreply.github.com>
2023-03-08 01:00:52 +0000

    Refactored map code

--- app/lib/Map/open_layers_map.dart
+++ app/lib/Map/open_layers_map.dart
@@ -6,52 +6,47 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-import '../MapData/bus_stop.dart';
 import '../MapData/feature.dart';
 import '../MapData/map_data.dart';
 import '../Permissions/location_permissions_handler.dart';
 
 /// Encapsulates the map created through open layers and the map.html file so
 /// that the rest of the dart code can interact with the ma easily.
-class OpenLayersMap
-{
+class OpenLayersMap {
   late final WebViewController _webViewController;
+
   // The function to be run whenever a marker is clicked.
   Function(String)? _markerClickedFunction;
 
   /// The constructor creating the webview controller and loading the map.
-  OpenLayersMap()
-  {
+  OpenLayersMap() {
     _webViewController = _createWebViewController();
     _loadMap();
   }
 
   /// Assigns a function to be run when a marker on the map is clicked.
-  onMarkerClicked(Function(String) markerClicked)
-  {
+  onMarkerClicked(Function(String) markerClicked) {
     _markerClickedFunction = markerClicked;
   }
 
   /// Returns the webview controller.
-  WebViewController getController()
-  {
+  WebViewController getController() {
     return _webViewController;
   }
 
   /// Toggles the visibility of the U1 bus stop markers on the map.
-  toggleMarkers(MapDataId layerId, bool visible)
-  {
+  toggleMarkers(MapDataId layerId, bool visible) {
     String jsObject = "{layerId: '${layerId.idPrefix}', visible: $visible}";
     _webViewController.runJavaScript("toggleShowMarkers($jsObject)");
   }
 
   // Creates the webview controller
-  WebViewController _createWebViewController()
-  {
+  WebViewController _createWebViewController() {
     return WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       // Creates the channel for javascript to call a dart method
-      ..addJavaScriptChannel("MarkerClickedDart", onMessageReceived: (JavaScriptMessage markerIdMessage) {
+      ..addJavaScriptChannel("MarkerClickedDart",
+          onMessageReceived: (JavaScriptMessage markerIdMessage) {
         _markerClicked(markerIdMessage.message);
       })
       ..setBackgroundColor(const Color(0x00000000))
@@ -64,61 +59,59 @@ class OpenLayersMap
           onPageFinished: (String url) {
             _assignLayerIds();
             // When the page finishes loading it sets the data to be loaded into the map.
-            MapDataLoader.getDataLoader().onDataLoaded((mapData)
-            {
+            MapDataLoader.getDataLoader().onDataLoaded((mapData) {
               _addMarkers(mapData);
             });
             _addUserLocationIcon();
           },
           onWebResourceError: (WebResourceError error) {},
-          onNavigationRequest: (NavigationRequest request) {return NavigationDecision.navigate;},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
         ),
       );
   }
 
   // Loads map.html from the respective platforms location.
-  _loadMap()
-  {
+  _loadMap() {
     String relativePath = "assets/open-layers-map/map.html";
     if (Platform.isAndroid) {
       _webViewController.loadFile(
           "file:///android_asset/flutter_assets/$relativePath"); // Android file path
-    }
-    else if (Platform.isIOS)
-    {
+    } else if (Platform.isIOS) {
       // TODO: Add the absolute path for ios
-      _webViewController.loadFile(
-          "$relativePath"); // IOS file path
+      _webViewController.loadFile("$relativePath"); // IOS file path
     }
   }
 
-  _assignLayerIds()
-  {
-    String jsObject = "{U1: '${MapDataId.u1.idPrefix}', U2: '${MapDataId.u2.idPrefix}', UniBuilding: '${MapDataId.uniBuilding.idPrefix}', Landmark: '${MapDataId.landmark.idPrefix}', UserLocation: '${MapDataId.userLocation.idPrefix}'}";
+  _assignLayerIds() {
+    String u1 = MapDataId.u1.idPrefix;
+    String u2 = MapDataId.u2.idPrefix;
+    String uniBuilding = MapDataId.uniBuilding.idPrefix;
+    String landmark = MapDataId.landmark.idPrefix;
+    String userLocation = MapDataId.userLocation.idPrefix;
+    String jsObject =
+        "{U1: '$u1', U2: '$u2', UniBuilding: '$uniBuilding', Landmark: '$landmark', UserLocation: '$userLocation'}";
     _webViewController.runJavaScript("mapIdsToLayers($jsObject)");
   }
 
   // Adds the markers to the map.
-  _addMarkers(MapData mapData)
-  {
-    for(Feature feature in mapData.getAllFeatures())
-    {
+  _addMarkers(MapData mapData) {
+    for (Feature feature in mapData.getAllFeatures()) {
       _addMarker(feature.typeId, feature.id, feature.long, feature.lat);
     }
   }
 
   // Adds the U1 bus stops.
-  _addMarker(MapDataId layerId, String id, double long, double lat)
-  {
-    String jsObject = "{layerId: '${layerId.idPrefix}', id: '$id', longitude: $long, latitude: $lat}";
+  _addMarker(MapDataId layerId, String id, double long, double lat) {
+    String jsObject =
+        "{layerId: '${layerId.idPrefix}', id: '$id', longitude: $long, latitude: $lat}";
     _webViewController.runJavaScript("addMarker($jsObject)");
   }
 
   // This is called when a marker on the map gets clicked.
-  _markerClicked(String markerId)
-  {
-    if (_markerClickedFunction == null)
-    {
+  _markerClicked(String markerId) {
+    if (_markerClickedFunction == null) {
       return;
     }
     _markerClickedFunction!(markerId);
@@ -126,19 +119,20 @@ class OpenLayersMap
 
   // Adds the users location to the map as a marker and sets for it be updated
   // whenever the user moves.
-  _addUserLocationIcon() async
-  {
-    LocationPermissionsHandler handler = LocationPermissionsHandler.getHandler();
+  _addUserLocationIcon() async {
+    LocationPermissionsHandler handler =
+        LocationPermissionsHandler.getHandler();
     Location location = handler.getLocation();
 
-    if (await handler.hasPermission())
-    {
+    if (await handler.hasPermission()) {
       LocationData currentLocation = await location.getLocation();
-      _addMarker(MapDataId.userLocation, MapDataId.userLocation.idPrefix, currentLocation.longitude!, currentLocation.latitude!);
+      _addMarker(MapDataId.userLocation, MapDataId.userLocation.idPrefix,
+          currentLocation.longitude!, currentLocation.latitude!);
     }
 
     location.onLocationChanged.listen((LocationData currentLocation) {
-      _addMarker(MapDataId.userLocation, MapDataId.userLocation.idPrefix, currentLocation.longitude!, currentLocation.latitude!);
+      _addMarker(MapDataId.userLocation, MapDataId.userLocation.idPrefix,
+          currentLocation.longitude!, currentLocation.latitude!);
     });
   }
 }
