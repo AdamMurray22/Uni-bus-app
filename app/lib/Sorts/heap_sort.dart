18302cb114dce7100b2089a16077962f6923e2ab
Adam-Murray <116890354+AdamMurrayPortsmouth@users.noreply.github.com>
2023-03-12 03:27:50 +0000

    Update heap_sort.dart

--- app/lib/Sorts/heap_sort.dart
+++ app/lib/Sorts/heap_sort.dart
@@ -6,6 +6,7 @@ class HeapSort
   /// Performs a heap sort on the given list using the given comparator.
   static List<E> sort<E>(Iterable<E> list,  ComparatorValue Function(E, E) compare)
   {
+    // TODO: Implement heap sort.
     return list.toList();
   }
 }
