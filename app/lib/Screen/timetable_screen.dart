765e01f5743350e43f8b8937157d33396dcd5123
Adam-Murray <116890354+AdamMurrayPortsmouth@users.noreply.github.com>
2023-03-23 23:30:10 +0000

    Update timetable_screen.dart

--- app/lib/Screen/timetable_screen.dart
+++ app/lib/Screen/timetable_screen.dart
@@ -23,37 +23,36 @@ class _TimetableScreenState extends State<TimetableScreen> {
     MapDataLoader.getDataLoader().onDataLoaded((mapData) {
       setState(() {
         // Adds all the bus stops to the timetable.
-        for (BusStop busStop in mapData.getAllBusStops())
-        {
-          if (busStop.getHasSeparateArrDepTimes())
-          {
-            _addBusStop("${busStop.name} (Arrivals)", busStop.getArrivalTimes());
-            _addBusStop("${busStop.name} (Departures)", busStop.getDepartureTimes());
-          }
-          else
-          {
+        for (BusStop busStop in mapData.getAllBusStops()) {
+          if (busStop.getHasSeparateArrDepTimes()) {
+            _addBusStop(
+                "${busStop.name} (Arrivals)", busStop.getArrivalTimes());
+            _addBusStop(
+                "${busStop.name} (Departures)", busStop.getDepartureTimes());
+          } else {
             _addBusStop(busStop.name, busStop.getDepartureTimes());
           }
         }
 
         // Sorts the bus stops so that the stops that the bus gets too first
         // appear at the top of the screen.
-        HeapSort<DropdownButton<String>> sort = HeapSort<DropdownButton<String>>((item1, item2)
-        {
-          List<DropdownMenuItem<String>> times1 = item1.items as List<DropdownMenuItem<String>>;
+        HeapSort<DropdownButton<String>> sort =
+            HeapSort<DropdownButton<String>>((item1, item2) {
+          List<DropdownMenuItem<String>> times1 =
+              item1.items as List<DropdownMenuItem<String>>;
           int hour1 = int.parse(times1[1].value!.substring(0, 2));
           hour1 == 0 ? hour1 = 24 : hour1;
           int minute1 = int.parse(times1[1].value!.substring(3, 5));
           int firstTime1 = minute1 + (hour1 * 60);
 
-          List<DropdownMenuItem<String>> times2 = item2.items as List<DropdownMenuItem<String>>;
+          List<DropdownMenuItem<String>> times2 =
+              item2.items as List<DropdownMenuItem<String>>;
           int hour2 = int.parse(times2[1].value!.substring(0, 2));
           hour2 == 0 ? hour2 = 24 : hour2;
           int minute2 = int.parse(times2[1].value!.substring(3, 5));
           int firstTime2 = minute2 + (hour2 * 60);
 
-          if (firstTime1 <= firstTime2)
-          {
+          if (firstTime1 <= firstTime2) {
             return Comparator.before;
           }
           return Comparator.after;
@@ -68,54 +67,73 @@ class _TimetableScreenState extends State<TimetableScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [...dropDownButtons,
-          RichText(
-            text: TextSpan(
-              children: [
-                const TextSpan(
-                  text: 'Buses only run on week days during term time excluding bank holidays, check the ',
-                  style: TextStyle(color: Colors.black),
-                ),
-                TextSpan(
-                  text: 'Uni Bus website',
-                  style: const TextStyle(color: Colors.blue),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      launchUrl(Uri(
-                          scheme: 'https',
-                          host: 'myport.port.ac.uk',
-                          path: '/life-at-uni/life-on-campus/getting-to-campus/university-buses'));
-                    },
-                ),
-                const TextSpan(
-                  text: ' for a complete list of dates.',
-                  style: TextStyle(color: Colors.black),
-                ),
-              ],
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+        const Padding(
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child:Text(
+              "The bus takes a one-way circular route, stopping at: ",
+              style: TextStyle(color: Colors.black),
+            )),
+            ...dropDownButtons,
+            RichText(
+              text: TextSpan(
+                children: [
+                  const TextSpan(
+                    text:
+                        'Buses only run on week days during term time excluding'
+                            ' bank holidays, check the ',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  TextSpan(
+                    text: 'Uni Bus website',
+                    style: const TextStyle(color: Colors.blue),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        launchUrl(Uri(
+                            scheme: 'https',
+                            host: 'myport.port.ac.uk',
+                            path:
+                                '/life-at-uni/life-on-campus/getting-to-campus'
+                                    '/university-buses'));
+                      },
+                  ),
+                  const TextSpan(
+                    text: ' for a complete list of dates.',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
             ),
-          ),],
+          ],
+        ),
       ),
     ));
   }
 
   // Adds a bus stop to the timetable.
-  _addBusStop(String busStopName, Iterable<BusTime> busTimes)
-  {
+  _addBusStop(String busStopName, Iterable<BusTime> busTimes) {
     List<DropdownMenuItem<String>> times = [];
-    times.add(DropdownMenuItem(value: busStopName, child: Text(busStopName),));
-    for (BusTime time in busTimes)
-    {
-      times.add(DropdownMenuItem(value: time.toDisplayString(), child: Text(time.toDisplayString()),));
+    times.add(DropdownMenuItem(
+      value: busStopName,
+      child: Text(busStopName),
+    ));
+    for (BusTime time in busTimes) {
+      times.add(DropdownMenuItem(
+        value: time.toDisplayString(),
+        child: Text(time.toDisplayString()),
+      ));
     }
     dropDownButtons.add(DropdownButton(
       value: busStopName,
       items: times,
       menuMaxHeight: 500,
-      onChanged: (value) {  },
+      onChanged: (value) {},
       elevation: 8,
       isExpanded: true,
     ));
