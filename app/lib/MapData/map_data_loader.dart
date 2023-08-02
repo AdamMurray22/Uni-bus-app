9f008ae8d44d8cca8d40f9d1c89c738b17fb7f26
Adam-Murray <adammurray1122@gmail.com>
2023-08-02 06:16:49 +0100

    Loading Screen now closes app and waits for map to load

--- app/lib/MapData/map_data_loader.dart
+++ app/lib/MapData/map_data_loader.dart
@@ -9,20 +9,17 @@ import 'map_data.dart';
 
 /// This loads the information from the backend and allows the frontend to
 /// access it.
-class MapDataLoader
-{
+class MapDataLoader {
   static MapDataLoader? _mapDataLoader;
   late MapData _data;
-  bool _loadingFinished = false;
-
+  bool? _loadingCompletedSuccessfully;
   final List<Function(MapData)> _dataLoadedFunctions = [];
   Function(bool)? _dataLoadingCompleted;
 
   MapDataLoader._();
 
   /// Returns the only object of MapDataLoader.
-  static MapDataLoader getDataLoader()
-  {
+  static MapDataLoader getDataLoader() {
     _mapDataLoader ??= MapDataLoader._();
     return _mapDataLoader!;
   }
@@ -30,65 +27,66 @@ class MapDataLoader
   /// This loads the information and if
   /// onDataLoaded() has been called it runs the
   /// function given to onDataLoaded().
-  load() async
-  {
+  load() async {
     loadFromLoader(DatabaseLoader.getDataBaseLoader());
   }
 
   /// This loads the information and if
   /// onDataLoaded() has been called it runs the
   /// function given to onDataLoaded().
-  loadFromLoader(DatabaseLoader loader) async
-  {
-    Tuple5<Set<Feature>, Map<String, int>, Map<String, List<BusTime>>, Map<String, List<BusTime>>,
-        BusRunningDates> mapData = await loader.load();
+  loadFromLoader(DatabaseLoader loader) async {
+    Tuple5<
+        Set<Feature>,
+        Map<String, int>,
+        Map<String, List<BusTime>>,
+        Map<String, List<BusTime>>,
+        BusRunningDates>? mapData;
+    try {
+      mapData = await loader.load();
+    } on Exception catch (_, e)
+    {
+      _loadingCompletedSuccessfully = false;
+      _dataLoadingCompleted!(false);
+      return;
+    }
     _data = MapData(mapData);
 
     // Sets _loadingFinished to true and runs any functions given to
     // MapDataLoader that should be run as soon as the data is loaded.
-    _loadingFinished = true;
-    for (Function(MapData) dataLoadedFunction in _dataLoadedFunctions)
-    {
+    _loadingCompletedSuccessfully = true;
+    for (Function(MapData) dataLoadedFunction in _dataLoadedFunctions) {
       dataLoadedFunction(_data);
     }
-    if (_dataLoadingCompleted != null)
-    {
+    if (_dataLoadingCompleted != null) {
       _dataLoadingCompleted!(true);
     }
   }
 
   /// Sets the function to be called once the information has finished being
   /// loaded, if it has already finished being loaded then it runs the function.
-  onDataLoaded(Function(MapData) dataLoaded)
-  {
+  onDataLoaded(Function(MapData) dataLoaded) {
     _dataLoadedFunctions.add(dataLoaded);
-    if (_loadingFinished)
-    {
-      dataLoaded(_data);
+    if (_loadingCompletedSuccessfully != null && _loadingCompletedSuccessfully!) {
+        dataLoaded(_data);
     }
   }
 
   /// Sets a function to be called when the data is loaded successfully or fails to be loaded.
-  onDataLoadingCompleted(Function(bool) loadingSuccessful)
-  {
+  onDataLoadingCompleted(Function(bool) loadingSuccessful) {
     _dataLoadingCompleted = loadingSuccessful;
-    if (_loadingFinished)
-    {
-      _dataLoadingCompleted!(true);
+    if (_loadingCompletedSuccessfully != null) {
+      _dataLoadingCompleted!(_loadingCompletedSuccessfully!);
     }
   }
 
   /// Returns whether the loading has finished.
-  bool loadingFinished()
-  {
-    return _loadingFinished;
+  bool loadingFinished() {
+    return _loadingCompletedSuccessfully != null;
   }
 
   /// Returns the map data. Only to be used once the data has already been loaded.
-  MapData getMapData()
-  {
-    if (_loadingFinished == false)
-    {
+  MapData getMapData() {
+    if (_loadingCompletedSuccessfully == null) {
       throw LoadingNotFinishedException("Data has not finished loading yet.");
     }
     return _data;
