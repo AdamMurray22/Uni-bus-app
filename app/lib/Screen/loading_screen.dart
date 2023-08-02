a8dd501c43916c1bdf60b1ae96a55572b778bf5d
Adam-Murray <adammurray1122@gmail.com>
2023-08-02 06:26:34 +0100

    improved how the app is closed from the loading screen

--- app/lib/Screen/loading_screen.dart
+++ app/lib/Screen/loading_screen.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../Map/map_widget.dart';
 import '../MapData/map_data_loader.dart';
@@ -48,7 +47,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
               child: const Text('Close'),
               onPressed: () {
                 Navigator.of(context).pop();
-                exit(0);
+                SystemChannels.platform.invokeMethod('SystemNavigator.pop');
               },
             ),
           ],
