302c91775b2694c8eff84e3a23acf22209d2fd8a
Adam-Murray <adammurray1122@gmail.com>
2023-08-05 23:25:04 +0100

    Fixed bug where bus geo json wouldnt display properly

--- app/lib/Routing/RouteCreators/bus_route_geo_json_trimmer.dart
+++ app/lib/Routing/RouteCreators/bus_route_geo_json_trimmer.dart
@@ -79,6 +79,10 @@ class BusRouteGeoJsonTrimmer
           Location(coordinates[i + 1][0], coordinates[i + 1][1]));
       if (distanceFromLocation < distanceFromLastCoordinate)
       {
+        print("");
+        print(distanceFromLocation);
+        print(distanceFromLastCoordinate);
+        print("");
         for (int n = i; n >= 0; n--) {
           coordinates.removeAt(n);
           i--;
--- app/lib/Routing/estimate_straight_line_distance.dart
+++ app/lib/Routing/estimate_straight_line_distance.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'location.dart';
 
@@ -7,11 +7,48 @@ class EstimateStraightLineDistance
   /// Estimates the straight line distance of 2 points in m.
   static double estimateStraightLineDistance(Location fromLocation, Location toLocation)
   {
-    double x = (fromLocation.getLatitude() - toLocation.getLatitude()) *
-        cos((fromLocation.getLongitude() + toLocation.getLongitude()) / 2);
-    double y = (fromLocation.getLongitude() - toLocation.getLongitude());
-    double d = sqrt(x * x + y * y) * 6371000;
-    d = d / 100;
-    return d;
+    return distance(fromLocation.getLatitude(), toLocation.getLatitude(),
+        fromLocation.getLongitude(), toLocation.getLongitude());
+  }
+
+  static double toRadians(
+      double angleIn10thofaDegree)
+  {
+    // Angle in 10th
+    // of a degree
+    return (angleIn10thofaDegree *
+        math.pi) / 180;
+  }
+  static double distance(double lat1,
+      double lat2,
+      double lon1,
+      double lon2)
+  {
+
+    // The math module contains
+    // a function named toRadians
+    // which converts from degrees
+    // to radians.
+    lon1 = toRadians(lon1);
+    lon2 = toRadians(lon2);
+    lat1 = toRadians(lat1);
+    lat2 = toRadians(lat2);
+
+    // Haversine formula
+    double dlon = lon2 - lon1;
+    double dlat = lat2 - lat1;
+    num a = math.pow(math.sin(dlat / 2), 2) +
+        math.cos(lat1) * math.cos(lat2) *
+            math.pow(math.sin(dlon / 2),2);
+
+    double c = 2 * math.asin(math.sqrt(a));
+
+    // Radius of earth in
+    // kilometers. Use 3956
+    // for miles
+    double r = 6371;
+
+    // calculate the result
+    return (c * r) * 1000;
   }
 }
