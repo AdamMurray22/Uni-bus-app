7c8864def48f68754c5a4a1635b6bb1e2132918a
Adam-Murray <adammurray1122@gmail.com>
2023-08-30 10:05:26 +0100

    Updated app icon and loading screen

--- app/lib/Screen/loading_screen.dart
+++ app/lib/Screen/loading_screen.dart
@@ -116,7 +116,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
-          color: const Color(0xff7030A0),
+          color: const Color(0xff009a96),
           shape: BoxShape.rectangle,
           borderRadius: BorderRadius.zero,
           border: Border.all(color: const Color(0x4d9e9e9e), width: 1),
@@ -125,24 +125,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.4),
-            const Text(
-              "University bus",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-            const Text(
-              "Portsmouth",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.4),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.27),
+            Image.asset('assets/loading-screen/App Icon.png',
+            height: MediaQuery.of(context).size.height * 0.3,
+            width: MediaQuery.of(context).size.width * 0.8,),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.25),
             const Text(
               "Loading...",
               style: TextStyle(
